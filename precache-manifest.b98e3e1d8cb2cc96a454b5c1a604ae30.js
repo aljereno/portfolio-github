bdf5dfa05509042904bb5803987fb74966bfd1c5
alereno <aljaedereno@gmail.com>
2020-09-05 22:20:43 -1000

    Updates

---
+++ precache-manifest.b98e3e1d8cb2cc96a454b5c1a604ae30.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8492aea694aaca6dd0353fa3db1e5c7e",
+    "url": "/portfolio-github/index.html"
+  },
+  {
+    "revision": "97009046a56fb64f7621",
+    "url": "/portfolio-github/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d72fdf9a93843a3e9806",
+    "url": "/portfolio-github/static/js/2.e42edcea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio-github/static/js/2.e42edcea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97009046a56fb64f7621",
+    "url": "/portfolio-github/static/js/main.b4294606.chunk.js"
+  },
+  {
+    "revision": "b7e7a1e6721bcf07169a",
+    "url": "/portfolio-github/static/js/runtime-main.3eec76d8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio-github/static/media/logo.5d5d9eef.svg"
+  }
+]);

aad2770a47db6e4870262c58eafc444efd0dd4fa
alereno <aljaedereno@gmail.com>
2021-01-12 20:26:00 -1000

    finished carousel and the list's for january and february goals

--- src/components/HeadCarousel.js
+++ src/components/HeadCarousel.js
@@ -1,55 +1,33 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Carousel from 'react-bootstrap/Carousel';
-//import useWindowDimensions from './WindowDimensions'
+import useFirestore from '../hooks/useFirestore';
 import '../styles/globalstyles.css';
 
 const HeadCarousel = () => {
 
     //const { height, width } = useWindowDimensions();
+    const { docs } = useFirestore('carousel-images');
 
     return (
         <Container>
           <Carousel>
-            <Carousel.Item className="carouselImages">
+            {docs && docs.map(doc => (
+              <Carousel.Item className="carouselImages">
               {/* The require keyword is needed*/}
               <img
                 className="d-block carouselImages"
-                src={require("../images/image1.jpg")}
+                src={(doc.url)}
                 alt="First slide"
               />
               <Carousel.Caption>
-                <h3>First slide label</h3>
+                <h3>{doc.title}</h3>
                 <p>
-                  Nulla vitae elit libero, a pharetra augue mollis interdum.
+                  {doc.caption}
                 </p>
               </Carousel.Caption>
             </Carousel.Item>
-            {/* <Carousel.Item>
-              <img
-                className="d-block carouselImages"
-                src={require("../images/image2.jpg")}
-                alt="First slide"
-              />
-              <Carousel.Caption>
-                <h3>Second slide label</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-              </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-              <img
-                className="d-block carouselImages"
-                src={require("../images/image3.jpg")}
-                alt="First slide"
-              />
-              <Carousel.Caption>
-                <h3>Third slide label</h3>
-                <p>
-                  Praesent commodo cursus magna, vel scelerisque nisl
-                  consectetur.
-                </p>
-              </Carousel.Caption>
-            </Carousel.Item> */}
+            ))}
           </Carousel>
         </Container>
     );

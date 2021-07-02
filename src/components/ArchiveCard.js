c0cef47050609cfd93e4cde8c6245e4082cf8b73
alereno <aljaedereno@gmail.com>
2021-07-02 04:06:20 -1000

    additions of cards for archive cards

--- src/components/ArchiveCard.js
+++ src/components/ArchiveCard.js
@@ -1,18 +1,14 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
-import Card from '../minis/Card';
 import useFirestore from '../hooks/useFirestore';
+import { Container } from 'react-bootstrap';
 
-const Entertainment = () => {
+const ArchiveCard = ({setModalImg}) => {
     const { docs } = useFirestore('entertainment-stories')
     return (
-        // <Container style={{backgroundColor: '#2e4a1e', borderRadius: "25px"}}>
-        //     <Card imageName="entertainment-stories"/>
-        // </Container>
         <Container style={{marginTop: "1rem"}}>
             <div className="entertain-grid">
             { docs && docs.map(doc => (
-                <div className="entertain-wrap" style={{backgroundColor: "gray", justifyContent: "center", alignContent: "center"}}>
+                <div className="entertain-wrap" style={{backgroundColor: "gray"}}>
                     <h1 style={{writingMode: "vertical-lr", backgroundColor: "coral"}}>{doc.title}</h1>
                     {/* {doc.url && <img src={doc.url} alt={doc.placement}/>}  */}
                 </div>
@@ -22,4 +18,4 @@ const Entertainment = () => {
     )
 }
 
-export default Entertainment;
+export default ArchiveCard;

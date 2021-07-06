5986c2ba5a9187e289e5523bb563fb1a23ca5d1e
alereno <aljaedereno@gmail.com>
2021-07-05 17:12:55 -1000

    added in acrhive file

---
+++ src/singlescreens/Archive.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+const Archive = () => {
+    return (
+        <Container>
+            <p>Test words</p>
+        </Container>
+    )
+}
+
+export default Archive;
